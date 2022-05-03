30bd9632daf5c0a3d278ebe72061799266071039
Mukund <mukunddtridhyatech@gmail.com>
2022-05-03 12:44:08 +0530

    edit main content file

--- src/app/Shared/material.module.ts
+++ src/app/Shared/material.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 
 const MaterialComponents = [
-    MatButtonModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatListModule
+  MatButtonModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatListModule,
+  MatCardModule,
 ];
 
 @NgModule({
-    imports: [ MaterialComponents ],
-    exports: [ MaterialComponents ],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
 })
-
 export class MaterialModule {}
