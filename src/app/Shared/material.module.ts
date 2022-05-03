f37eb80d6d7aa37fb4a9f817ef6466e8308207ab
Mukund <mukunddtridhyatech@gmail.com>
2022-05-03 10:10:22 +0530

    create invoice-builder module, routing, component

--- src/app/Shared/material.module.ts
+++ src/app/Shared/material.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
 
 const MaterialComponents = [
     MatButtonModule,
+    MatIconModule,
 ];
 
 @NgModule({
