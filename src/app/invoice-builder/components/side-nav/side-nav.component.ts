3087547e84690645163ebc1ee0cb11bcb231bafc
Mukund <mukunddtridhyatech@gmail.com>
2022-05-03 12:40:55 +0530

    add scss and condition

--- src/app/invoice-builder/components/side-nav/side-nav.component.ts
+++ src/app/invoice-builder/components/side-nav/side-nav.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, NgZone, OnInit } from '@angular/core';
+const MAX_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
+  private mediaMatcher: MediaQueryList = matchMedia(
+    `(max-width: ${MAX_WIDTH_BREAKPOINT}px)`
+  );
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(zone: NgZone) {
+    this.mediaMatcher.addListener((mql) => {
+      zone.run(
+        () =>
+          (this.mediaMatcher = matchMedia(
+            `(max-width: ${MAX_WIDTH_BREAKPOINT}px)`
+          ))
+      );
+    });
   }
 
+  ngOnInit(): void {}
+
+  isScreenSmall() {
+    return this.mediaMatcher.matches;
+  }
 }
