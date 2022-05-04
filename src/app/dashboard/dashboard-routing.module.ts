3cd4543885d94d1938fe58b1f10f4ed0641b0e5e
Mukund <mukunddtridhyatech@gmail.com>
2022-05-04 15:03:01 +0530

    detete main-content file and edit routing

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,5 @@
 import { ClientListingComponent } from './../clients/components/client-listing/client-listing.component';
 import { InvoiceListingComponent } from './../invoices/components/invoice-listing/invoice-listing.component';
-import { MainContentComponent } from './components/main-content/main-content.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
@@ -11,16 +10,16 @@ const routes: Routes = [
     component: DashboardComponent,
     children: [
       {
-        path: '',
-        component: MainContentComponent,
-      },
-      {
         path: 'invoices',
         component: InvoiceListingComponent
       },
       {
         path: 'clients',
         component: ClientListingComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'invoices'
       }
     ],
   },
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,8 +3,6 @@ import { InvoicesModule } from './../invoices/invoices.module';
 import { MaterialModule } from './../Shared/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { MainContentComponent } from './components/main-content/main-content.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -14,7 +12,6 @@ import { DashboardComponent } from './dashboard.component';
 @NgModule({
   declarations: [
     DashboardComponent,
-    MainContentComponent,
     SideNavComponent,
     ToolbarComponent
   ],
